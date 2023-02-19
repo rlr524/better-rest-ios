722f62cf3cc1ab32f41bc74a39491d33f27e15b9
Rob Ranf <rob@emiyaconsulting.com>
2023-02-19 11:15:45 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rob Ranf on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
