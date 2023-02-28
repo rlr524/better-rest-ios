9c50e5446cce1b70ba7aac30ed423bc310145e6b
Rob Ranf <rob@emiyaconsulting.com>
2023-02-27 18:36:56 -0800

    Updated to remove calc button and always show result, updated in realtime.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,62 +10,82 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
-    @State private var alertTitle = ""
-    @State private var alterMessage = ""
-    @State private var showingAlert = false
-        
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,
-                        in: 4...12, step: 0.25)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount,
-                        in: 1...20)
-            }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") {}
-            } message: {
-                Text(alterMessage)
-            }
-        }
+//    @State private var alertTitle = ""
+//    @State private var alterMessage = ""
+//    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
     }
     
-    func calculateBedtime() {
+    var sleepResults: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let prediction = try model.prediction(wake: Double(hour + minute),
+                                                  estimatedSleep: sleepAmount,
+                                                  coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is..."
-            alterMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-            
+            return "Your ideal bedtime is " + sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
-            alterMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Error: Sorry, there was a problem calculating your bedtime."
         }
-        showingAlert = true
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    DatePicker("Please enter a time", selection: $wakeUp,
+                               displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                }
+                
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,
+                            in: 4...12, step: 0.25)
+                    
+                } header: { Text("Desired amount of sleep")
+                        .font(.headline)
+                }
+                
+                Section {
+                //Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                //value: $coffeeAmount, in: 1...20)
+                    Picker("Cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) { n in
+                            Text("\(n)")
+                        }
+                    }
+                } header: {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                }
+                
+                Text(sleepResults)
+                    .font(.title3)
+            }
+            .navigationTitle("BetterRest")
+//            .toolbar {
+//                Button("Calculate", action: calculateBedtime)
+//            }
+//            .alert(alertTitle, isPresented: $showingAlert) {
+//                Button("OK") {}
+//            } message: {
+//                Text(alterMessage)
+//            }
+        }
     }
 }
 
